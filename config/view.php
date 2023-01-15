4cf95249df329840dd2909f42c12f24b2c7f8220
ryuujin <mirchaemanuel@gmail.com>
2023-01-15 17:39:30 +0100

    fixing blade view not found in phar executable

--- config/view.php
+++ config/view.php
@@ -1,12 +1,8 @@
 <?php
-
 return [
     'paths' => [
         resource_path('views'),
     ],
 
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
-    ),
+    'compiled' => getcwd()
 ];
