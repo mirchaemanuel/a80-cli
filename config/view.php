a2aa093a4b331536c29c85cf63174ea4b4a7d1b3
ryuujin <mirchaemanuel@gmail.com>
2023-01-16 13:27:06 +0100

    view cache directory in a80 home dir

--- config/view.php
+++ config/view.php
@@ -1,10 +1,13 @@
 <?php
+
+use Illuminate\Support\Facades\File;
+
 return [
     'paths' => [
         resource_path('views'),
     ],
 
     'compiled' => \Phar::running()
-        ? getcwd()
+        ? $_SERVER['HOME'].'/.a80_cli/cache/views'
         : env('VIEW_COMPILED_PATH', realpath(storage_path('framework/views'))),
 ];
